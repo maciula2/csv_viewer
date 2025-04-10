859962ea2b1f0fd691feaf250a039fb9b2fb1f00
BladeRunner242 <macixxo@gmail.com>
2025-04-10 10:07:02 +0200

    Initialize Django project with CSV viewer functionality, including models, views, forms, and templates for file upload and data display.

---
+++ csv_viewer/viewer/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+import pandas as pd
+from .forms import UploadFileForm
+
+def upload_file(request):
+    form = UploadFileForm()
+    
+    df_html = None
+    row_count = None
+    column_names = None
+    column_types = None
+
+    form = UploadFileForm(request.POST, request.FILES)
+    if form.is_valid():
+        try:
+            csv_file = request.FILES['file']
+            df = pd.read_csv(csv_file)
+            row_count = len(df)
+            column_names = list(df.columns.values)
+            column_types = dict(df.dtypes)
+            while len(df) >= 50:
+                df = df.drop(len(df)-1)
+            df_html = df.to_html(classes='table table-bordered table-hover table-sm table-striped',
+                                index=False,
+                                border=0,
+                                justify='center',
+                                escape=False)
+        except Exception as e:
+            df_html = f"<p style='color:red;'>Error reading file: {e}</p>"
+    return render(request, 'viewer/upload.html', {'form': form, 'df_html': df_html, 'row_count': row_count, 'column_names': column_names, 'column_types': column_types})
